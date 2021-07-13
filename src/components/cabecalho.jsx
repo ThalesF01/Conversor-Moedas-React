9dcf15a7ee8e36ff7bd4bf8cbdb4667de51a3c15
Thales Fiscus <thalesgabriel07@gmail.com>
2021-07-13 00:30:28 -0300

    teste3

--- src/components/cabecalho.jsx
+++ src/components/cabecalho.jsx
@@ -6,6 +6,7 @@ export default function cabecalho(){
     return (
         <div className='cabeçalho'>
             <h1>Conversor Digital de Moedas</h1>
+            <img src="https://image.flaticon.com/icons/png/512/16/16302.png" />
             <p>Feito em base de conversão do dia 06/07/2021</p>
             <Relogio />
         </div>
