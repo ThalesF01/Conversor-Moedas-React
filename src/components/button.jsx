91e954d6f31857aa8b4ef76238b998ed18a0afa2
Thales Fiscus <thalesgabriel07@gmail.com>
2021-07-13 00:21:09 -0300

    sinal

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -6,12 +6,12 @@ export default function button(props){
     var resultado = 0
 
     function Calculator(){
-        if (props.moeda1 == 'Real' && props.moeda2 == 'Dolar')  resultado = props.valor * 0.20
-        else if(props.moeda1 == 'Real' && props.moeda2 == 'Euro') resultado = props.valor * 0.17
-        else if(props.moeda1 == 'Dolar' && props.moeda2 == 'Real') resultado = props.valor * 5.09
-        else if(props.moeda1 == 'Dolar' && props.moeda2 == 'Euro') resultado = props.valor * 0.84
-        else if(props.moeda1 == 'Euro' && props.moeda2 == 'Real') resultado = props.valor * 6.05
-        else if(props.moeda1 == 'Euro' && props.moeda2 == 'Dolar') resultado = props.valor * 1.19    
+        if (props.moeda1 === 'Real' && props.moeda2 === 'Dolar')  resultado = props.valor * 0.20
+        else if(props.moeda1 === 'Real' && props.moeda2 === 'Euro') resultado = props.valor * 0.17
+        else if(props.moeda1 === 'Dolar' && props.moeda2 === 'Real') resultado = props.valor * 5.09
+        else if(props.moeda1 === 'Dolar' && props.moeda2 === 'Euro') resultado = props.valor * 0.84
+        else if(props.moeda1 === 'Euro' && props.moeda2 === 'Real') resultado = props.valor * 6.05
+        else if(props.moeda1 === 'Euro' && props.moeda2 === 'Dolar') resultado = props.valor * 1.19    
     }
 
     return(
