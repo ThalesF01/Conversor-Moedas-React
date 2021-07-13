6e8540a5d0a187a068599adf3053800c00f41161
Thales Fiscus <thalesgabriel07@gmail.com>
2021-07-13 00:27:59 -0300

    teste2

--- src/components/cabecalho.jsx
+++ src/components/cabecalho.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Relogio from './relogio'
 import '../css/cabecalho.css'
 
-export default() =>
+export default function cabecalho(){
+    return (
         <div className='cabeçalho'>
             <h1>Conversor Digital de Moedas</h1>
-            <img src="https://image.flaticon.com/icons/png/512/16/16302.png" />
             <p>Feito em base de conversão do dia 06/07/2021</p>
             <Relogio />
         </div>
+    )
+}
