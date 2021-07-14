0aa23d6a427b437cb352e2503f74ab02e8ef1447
Thales Fiscus <thalesgabriel07@gmail.com>
2021-07-14 06:40:51 -0300

    Hist feito 1.0

--- src/components/HistMoeda.jsx
+++ src/components/HistMoeda.jsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import '../css/hist.css'
 
 export default function HistMoeda(props) {
 
     function Hist(moeda){
-        switch(moeda){
-            case 'Real': return <div className="HistMoeda">
+        if(moeda === 'Real'){ 
+            return <>
                 <h1>{props.moeda}</h1>
                 <p>O Real (ISO 4217: BRL, abreviado como R$) é a moeda corrente oficial da 
                     República Federativa do Brasil. Após sucessivas trocas monetárias, o Brasil 
@@ -14,18 +15,20 @@ export default function HistMoeda(props) {
                     Henrique Cardoso, depois eleito presidente da República. Quando o Real foi 
                     lançado, em 1 de julho, o ministro da Fazenda já era Rubens Ricupero, uma vez 
                     que FHC já tinha saído para desincompatibilizar-se e ter o direito de se 
-                    candidatar a Presidente da República.</p>;
-             </div>
-             
-             case 'Dolar': return <div className="HistMoeda">
+                    candidatar a Presidente da República.</p>
+             </>
+        }
+        else if(moeda === 'Dolar'){
+            return <>
                 <h1>{props.moeda}</h1>
                 <p>O dólar dos Estados Unidos (em inglês: United States dollar), também conhecido como dólar 
                     estadunidense ou dólar americano é a moeda oficial dos Estados Unidos e utilizada no mundo inteiro, 
                     tanto em reservas internacionais, como em livre circulação em alguns países. Atualmente, sua expedição 
-                    é controlada pela Reserva Federal dos Estados Unidos.</p>;
-             </div>
-
-            case 'Euro': return <div className="HistMoeda">
+                    é controlada pela Reserva Federal dos Estados Unidos.</p>
+             </>
+        }
+        else if(moeda === 'Euro'){
+            return <>
                 <h1>{props.moeda}</h1>
                 <p>Euro (símbolo: €; código: EUR) é a moeda oficial da zona Euro, a qual é constituída por 19 dos 27 
                     estados-membro da União Europeia: Alemanha, Áustria, Bélgica, Chipre, Eslováquia, Eslovénia, Espanha, 
@@ -33,23 +36,24 @@ export default function HistMoeda(props) {
                     e Portugal. A moeda é também usada de forma oficial pelas instituições da União Europeia e por 
                     quatro outros países europeus e, de forma unilateral, por outros dois. Em 2018, a moeda era usada 
                     diariamente por cerca de 343 milhões de europeus. A moeda é também usada oficialmente em diversos 
-                    territórios ultramarinos da UE.</p>;
-            </div>
-
-            case 'Libra': return <div className="HistMoeda">
-                <h1>{props.moeda}</h1>
+                    territórios ultramarinos da UE.</p>
+            </>
+        }
+        else if(moeda === 'Libra'){
+            return <>
+            <h1>{props.moeda}</h1>
                 <p>A libra esterlina ou simplesmente libra (em inglês, pound, plural pounds, informal. Pound Sterling, 
                     ou pounds sterling, formal) é a moeda oficial do Reino Unido. Desde 15 de Fevereiro de 1971 e da 
                     adoção do sistema decimal, ela é dividida em 100 pence (singular: penny). Antes dessa data, uma libra 
                     esterlina valia 20 shillings (que valiam por sua vez 12 pence cada um), ou 240 pence. A palavra libra (pound) 
                     era um termo utilizado pelas unidades monetárias de várias regiões da Europa. A palavra deriva do baixo latim libra 
-                    (século X). Originalmente, esse valor correspondia a uma libra de prata.</p>;
-            </div>
+                    (século X). Originalmente, esse valor correspondia a uma libra de prata.</p>
+            </>
         }
     }
 
     return(
-        <div>
+        <div className="HistMoeda">
             {Hist(props.moeda)}
         </div>
     )
